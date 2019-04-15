086e818fc6f7623eed2e8f121b74992ac23282b4
Mingna Li <mingnali@10-17-154-74.dynapool.wireless.nyu.edu>
2019-04-15 12:48:03 -0400

    added files

---
+++ week2/partB/index.js
@@ -0,0 +1,30 @@
+let express = require('express')
+let mustache = require('mustache-express')
+
+let app = express()
+
+app.engine('html', mustache())
+app.set('view engine', 'html');
+app.set('views', __dirname);
+
+app.get('/', function (req, res) {
+  res.render('index', {
+    'name': 'Mingna'
+  })
+})
+
+app.get('/submit', function (req, res) {
+  res.render('index', {
+    'result': req.query.bandName
+  })
+})
+
+app.get('/submit', function (req, res) {
+  res.render('part-b', {
+    'insult': insult
+  })
+})
+
+app.listen(8000, function () {
+  console.log("sever is up")
+})
