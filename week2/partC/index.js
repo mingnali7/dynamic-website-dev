3fb4ad254f6d77b74c339cc6a0eee56880f6d48f
Mingna Li <mingnali@10-17-154-74.dynapool.wireless.nyu.edu>
2019-04-15 13:37:44 -0400

    partC is working

---
+++ week2/partC/index.js
@@ -0,0 +1,15 @@
+let express = require('express')
+let app = express()
+let path = require('path')
+
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')))
+
+app.get('/submit', function (req, res) {
+  let website = req.query.url
+  console.log(website)
+  res.redirect(website)
+})
+
+
+app.listen(8000, () => console.log('listening on port 8000!'))
