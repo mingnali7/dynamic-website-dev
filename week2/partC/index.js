fd7efd4d85e845c3c49e7aabe64855e99981ec2c
Mingna Li <mingnali@10-17-154-74.dynapool.wireless.nyu.edu>
2019-04-15 13:39:08 -0400

    cleaned the code

--- week2/partC/index.js
+++ week2/partC/index.js
@@ -7,9 +7,7 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')))
 
 app.get('/submit', function (req, res) {
   let website = req.query.url
-  console.log(website)
   res.redirect(website)
 })
-
 
 app.listen(8000, () => console.log('listening on port 8000!'))
